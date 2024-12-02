6a1c32f7851c3da149131f82b1595ade3f5c1293
Abhishek Pawar <abhishekpawar131202@gmail.com>
2024-12-03 01:35:52 +0530

    changes

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -6,6 +6,7 @@ import { singleUpload } from "../middlewares/multer.js";
 const router = express.Router();
 
 router.route("/register").post(singleUpload, register);
+
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, singleUpload, updateProfile);
