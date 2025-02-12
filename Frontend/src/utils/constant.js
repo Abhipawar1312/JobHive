02d7a38e85c2454916320be3de0deae21dbb3bef
Abhishek Pawar <abhishekpawar131202@gmail.com>
2025-02-13 01:13:51 +0530

    changes

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT = "https://jobhive-m79b.onrender.com//api/v1/user";
-export const JOB_API_END_POINT = "https://jobhive-m79b.onrender.com//api/v1/job";
-export const APPLICATION_API_END_POINT = "https://jobhive-m79b.onrender.com//api/v1/application";
-export const COMPANY_API_END_POINT = "https://jobhive-m79b.onrender.com//api/v1/company";
-export const SAVEDJOB_API_END_POINT = "https://jobhive-m79b.onrender.com//api/v1/savedjobs";
+export const USER_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/company";
+export const SAVEDJOB_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/savedjobs";
 // export const SAVEDJOB_API_END_POINT = "http://localhost:8000/api/v1/savedjobs";
