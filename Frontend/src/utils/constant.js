288153e810b0d1ffcd944d502f9f681f2a18dcea
Abhishek Pawar <abhishekpawar131202@gmail.com>
2025-02-14 00:26:58 +0530

    url changes

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,14 +1,14 @@
-// export const USER_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/user";
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/user";
+// export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
 
-// export const JOB_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/job";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/job";
+// export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
 
-// export const APPLICATION_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/application";
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
+export const APPLICATION_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/application";
+// export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
 
-// export const COMPANY_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/company";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const COMPANY_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/company";
+// export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
 
-// export const SAVEDJOB_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/savedjobs";
-export const SAVEDJOB_API_END_POINT = "http://localhost:8000/api/v1/savedjobs";
+export const SAVEDJOB_API_END_POINT = "https://jobhive-m79b.onrender.com/api/v1/savedjobs";
+// export const SAVEDJOB_API_END_POINT = "http://localhost:8000/api/v1/savedjobs";
